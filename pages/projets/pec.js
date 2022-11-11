0604cb7dd75779160dbdecec4f67b4b47d655127
Ewan Rakotoanosy <55093676+rakotoew@users.noreply.github.com>
2022-11-12 00:03:21 +0100

    delete link to video
    delete of link to video for review

--- pages/projets/pec.js
+++ pages/projets/pec.js
@@ -8,7 +8,6 @@ export default function pec() {
                 <h2>Projet Universitaire</h2>
                 <h3>Private Encrypted cloud est un service cloud sécuriser avec un cryptage bout-en-bout avec React.js pour le Front, et Nest.js pour le Back</h3>
                 <h4>Réalisé en 2021-2022</h4>
-                <video controls src={"/video/pec.mp4"} style={{ width: "100%", height: "auto"}}/>
                 <h3>Projet réalisé sur 1 an avec un équipe de 5 personne, en méthode Agile avec des sprints d&apos;1 à 2 semaines (avec utilisation de Scrumwise pour la gestion de projet)</h3>
                 <h2>Technologies utilisées :</h2>
                 <h3>- React</h3>
