44743f527bbc6a47bd87f5680260cdb6716632b0
Ewan Rakotoanosy <55093676+rakotoew@users.noreply.github.com>
2022-11-11 23:21:47 +0100

    edit projects pages

--- pages/projets.js
+++ pages/projets.js
@@ -6,28 +6,37 @@ export default function projets () {
     return(
         <Layout>
             <div>
-            <h1>Mes projets notables :</h1>
-            <div class={styles.cardHolder}>
-                <Link href="/projets/pec" >
-                    <div class={styles.card}>
-                        <h2>Private Encrypted Cloud</h2>
-                        <span>TypeScript, React/Nest</span>
-                        <p>
-                            Project d&apos;un cloud avec un chiffrement bout-en-bout pour stocker et
-                            partager des données de façon sécurisée
-                        </p>
-                    </div>
-                </Link>
-                <Link href="/projets/byod">
-                    <div class={styles.card}>
-                        <h2>BYOD</h2>
-                        <span className="blue">C#, Unity</span>
-                        <p>
-                            Jeux sérieux réalisé avec Unity dans le but d&apos;avoir un jeux crossplatform
-                        </p>
-                    </div>
-                </Link>
-            </div>
+                <h1>Mes projets notables :</h1>
+                <div class={styles.cardHolder}>
+                    <Link href="/projets/pec" >
+                        <div class={styles.card}>
+                            <h2>Private Encrypted Cloud</h2>
+                            <span>TypeScript, React/Nest</span>
+                            <p>
+                                Project d&apos;un cloud avec un chiffrement bout-en-bout pour stocker et
+                                partager des données de façon sécurisée
+                            </p>
+                        </div>
+                    </Link>
+                    <Link href="/projets/byod">
+                        <div class={styles.card}>
+                            <h2>BYOD</h2>
+                            <span className="blue">C#, Unity</span>
+                            <p>
+                                Jeux sérieux réalisé avec Unity dans le but d&apos;avoir un jeux multijoueurs crossplatform
+                            </p>
+                        </div>
+                    </Link>
+                    <Link href="https://www.rakotoew.fr">
+                        <div class={styles.card}>
+                            <h2>Personnal webpage</h2>
+                            <span className="blue">Javascript, Next</span>
+                            <p>
+                                Mon portfolio présentant mes passions, mes compétences, et certains projet que j&apos;ai pu réalisé
+                            </p>
+                        </div>
+                    </Link>
+                </div>
             </div>
         </Layout>
     )
--- pages/projets/pec.js
+++ pages/projets/pec.js
@@ -7,7 +7,14 @@ export default function pec() {
                 <h1>Private Encrypted Cloud</h1>
                 <h2>Projet Universitaire</h2>
                 <h3>Private Encrypted cloud est un service cloud sécuriser avec un cryptage bout-en-bout avec React.js pour le Front, et Nest.js pour le Back</h3>
-                <video controls src={"/video/pec.mp4"} style={{ width: "100%", height: "35em"}}/>
+                <h4>Réalisé en 2021-2022</h4>
+                <video controls src={"/video/pec.mp4"} style={{ width: "100%", height: "auto"}}/>
+                <h3>Projet réalisé sur 1 an avec un équipe de 5 personne, en méthode Agile avec des sprints d&apos;1 à 2 semaines (avec utilisation de Scrumwise pour la gestion de projet)</h3>
+                <h2>Technologies utilisées :</h2>
+                <h3>- React</h3>
+                <h3>- Tailwind</h3>
+                <h3>- Nest</h3>
+                <h3>- TypeOrm</h3>
             </div>
         </Layout>
     )
