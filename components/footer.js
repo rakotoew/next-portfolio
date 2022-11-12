949c7085d5cde9a8d4a90bbca9f6b96517e3e971
Ewan Rakotoanosy <55093676+rakotoew@users.noreply.github.com>
2022-11-12 16:41:48 +0100

    Add more responsivness to the design

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,24 @@
 import styles from "../styles/components/Footer.module.css";
 import Image from 'next/image';
 
+var width = 2500;
+if (typeof window !== 'undefined') {
+    width = window.innerWidth;
+
+}
+if (width<1920){
+    width=1920;
+}
+var height = 100 * (2500/width);
+height = Math.round(height);
+
 export default function Footer(){
-    return(<footer>
-        <Image title="footer" alt="footer bg" src="/footer-img-small.svg" width="2500" height="100"/>
-        <div class={styles.content}>
-            <span>Ce site web est sous Licence GNU GPL 3.0</span>
-        </div>
+    return(<footer className={styles.foot}>
+        <Imagefooter title={"footer"} alt={"footer bg"} source={"/footer-img-small.svg"}/>
+        <span className={styles.content}>Ce site web est sous Licence GNU GPL 3.0</span>
     </footer>);
+}
+
+function Imagefooter({title, alt, source}) {
+    return (<Image title={title} alt={alt} src={source} width={String(width)} height={String(height)}/>)
 }
--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,26 @@ import Image from 'next/image';
 export default function Home() {
   return (
       <Layout>
-          <div>
-            <div class={styles.desc}>
-                <Image
-                    title="Ewan Rakotoanosy au Wicklow Moutains National Park"
-                    alt="Profile Picture"
-                    class={styles.profile}
-                    src="/profile.png"
-                    width="250"
-                    height="250"
-                />
-                <div>
+            <div className={styles.desc}>
+                <div className={styles.part}>
+                    <Image
+                        title="Ewan Rakotoanosy au Wicklow Moutains National Park"
+                        alt="Profile Picture"
+                        className={styles.profile}
+                        src="/profile.png"
+                        width="250"
+                        height="250"
+                    />
+                    <div className={styles.}>
+
+                    </div>
+                </div>
+                <div className={styles.part}>
                     <h1>Ewan Rakotoanosy</h1>
                     <h2>Développeur Full Stack</h2>
                     <h3>Etudiant en Master Informatique à l&apos;Université Savoie Mont Blanc</h3>
                 </div>
             </div>
-          </div>
       </Layout>
   )
 }
