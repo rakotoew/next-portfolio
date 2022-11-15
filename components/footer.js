9481a84b9293d2a343c5af80e0cdd3ebda3dc947
Ewan Rakotoanosy <55093676+rakotoew@users.noreply.github.com>
2022-11-15 21:36:46 +0100

    small fix on Footer

--- components/footer.js
+++ components/footer.js
@@ -3,19 +3,12 @@ import Image from 'next/image';
 import Link from "next/link";
 
 var width = 2500;
-if (typeof window !== 'undefined') {
-    width = window.innerWidth;
-
-}
-if (width<1920){
-    width=1920;
-}
 var height = 100 * (2500/width);
 height = Math.round(height);
 
 export default function Footer(){
     return(<footer className={styles.foot}>
-        <Imagefooter title={"footer"} alt={"footer bg"} source={"/footer-img-small.svg"}/>
+        <Imagefooter className={styles.img} title={"footer"} alt={"footer bg"} source={"/footer-img-small.svg"}/>
         <span className={styles.content}>
             Ce site web est sous <Link href="https://github.com/rakotoew/next-portfolio/blob/main/LICENSE.md">Licence GNU GPL v3.0</Link>
 
